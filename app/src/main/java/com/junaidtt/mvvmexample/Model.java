e2b26b5f313897b97450d111b93bb51f14c9c877
junaideveloper@gmail.com <you@example.com>
2021-06-07 23:14:10 +0400

    first commit

---
+++ app/src/main/java/com/junaidtt/mvvmexample/Model.java
@@ -0,0 +1,31 @@
+package com.junaidtt.mvvmexample;
+
+public class Model {
+
+
+    String email,password;
+
+    public Model(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
